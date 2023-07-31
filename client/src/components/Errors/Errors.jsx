15a76ed8e4914ebd020e4fd01a038c05a81249c6
Cintia Coccia <cintia_08@hotmail.com>
2023-07-31 02:05:12 -0300

    Errors

--- client/src/components/Errors/Errors.jsx
+++ client/src/components/Errors/Errors.jsx
@@ -1,5 +1,21 @@
-import styles from "../Errors/Errors.module.css";
+import styles from "./Errors.module.css";
+import Lottie from "lottie-react";
+import error from "./images/error.json";
+import { useEffect, useRef } from "react";
 
 export default function Errors(props) {
-    return <div>No se encuentran resultados</div>;
+    const lottie = useRef();
+
+    const { message } = props;
+
+    useEffect(() => {
+        lottie.current.setSpeed(1);
+    }, []);
+
+    return (
+        <div className={styles.errorContainer}>
+            <Lottie className={styles.errorImage} animationData={error} lottieRef={lottie} loop={true} />
+            <div className={styles.errorTitle}>{message}</div>
+        </div>
+    );
 }
